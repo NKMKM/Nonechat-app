58ce48b22ec25b32ed5754171fa5cda0a0c08bd2
NKMKM <77494915+NKMKM@users.noreply.github.com>
2022-06-10 12:02:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT || 5000
 const DB ="mongodb+srv://admin:admin@cluster0.wjm6i.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(DB, {
@@ -26,8 +25,8 @@ mongoose.connect(DB, {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(port, () =>
-  console.log(`Server started on ${port}`)
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
 );
 const io = socket(server, {
   cors: {
