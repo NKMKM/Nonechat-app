d8e80027ba027a050e90849c51fb99520ef66a58
NKMKM <77494915+NKMKM@users.noreply.github.com>
2022-06-10 11:43:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-
+const port = process.env.PORT || 5000
 const DB ="mongodb+srv://admin:admin@cluster0.wjm6i.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(DB, {
